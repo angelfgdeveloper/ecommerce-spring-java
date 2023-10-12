488e198468c30f17c9daeeb269e0d941789bee61
Luis Angel <angelfgdeveloper@gmail.com>
2023-10-11 20:27:18 -0600

    Aplicar permisos de rutas de requestMatchers

--- src/main/java/com/angelfg/ecommerce/web/config/SecurityConfig.java
+++ src/main/java/com/angelfg/ecommerce/web/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.angelfg.ecommerce.web.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,13 +16,18 @@ public class SecurityConfig {
                 .csrf(csrf -> csrf.disable()) // Proteccion anti-ataques de sitios no seguros
                 .cors(Customizer.withDefaults())
                 .authorizeRequests()
+                .requestMatchers(HttpMethod.GET, "/api/**").permitAll() // Permita todos los GET sin auth basic
+                .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll() // Permita todos especificos GET sin auth basic
+                .requestMatchers(HttpMethod.PUT).denyAll() // Deniega todos los metodos PUT
                 .anyRequest()
-//                .permitAll();
                 .authenticated()
                 .and()
                 .httpBasic(Customizer.withDefaults()); // base64 => user:password
 
         return http.build();
     }
+
+    // "/api/*" => permite hasta un nivel ejemplo http://localhost:8080/api/users
+    // "/api/**" => permite consumir todos http://localhost:8080/api/products/list?page=0&elements=1
 
 }
